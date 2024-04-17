30efff3612efcc25366fcccabaa7a358b689d555
Michael Bright <1880109+mjbright@users.noreply.github.com>
2024-04-17 13:46:53 +0000

    Added more commands ...

--- labs.py/examples/1.no-probes-pod-status/show_pod_lifecycle.sh
+++ labs.py/examples/1.no-probes-pod-status/show_pod_lifecycle.sh
@@ -1,2 +1,19 @@
+
+# Get the name of one of the Pods:
 POD1=$( kubectl get pods -l app=1-no-probes-pod-status --no-headers | awk '{ print $1; exit(0); }' )
+
+echo; echo "See Pod status transitions (in status: section):"
+set -x
 kubectl get pod $POD1 -o yaml | grep -A40 '^status:' | grep -E 'TransitionTime:|type:'
+set +x
+
+echo; echo "See Pod events (from kubectl describe pod):"
+set -x
+kubectl describe pod $POD1 | grep -A40 '^Events:'
+set +x
+
+echo; echo "See Pod events (from kubectl get events):"
+set -x
+kubectl events --for pod/$POD1
+set +x
+
