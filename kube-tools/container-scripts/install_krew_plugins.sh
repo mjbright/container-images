8e8e365a7a528cc2c140cf2fea96fe6f16dc1507
Michael Bright <1880109+mjbright@users.noreply.github.com>
2026-07-29 15:31:08 +0000

    Added 'ephemeral-storage' comment

--- kube-tools/container-scripts/install_krew_plugins.sh
+++ kube-tools/container-scripts/install_krew_plugins.sh
@@ -1,3 +1,17 @@
 
+echo "Error: Need to create Pod with extra ephemeral storage"
+echo
+
+cat <<EOF
+Events:
+  Type     Reason   Age   From     Message
+  ----     ------   ----  ----     -------
+  Warning  Evicted  11m   kubelet  The node was low on resource: ephemeral-storage. Threshold quantity: 2328468572, available: 1699532Ki. Container kube-tools was using 7101408Ki, request is 0, has larger consumption of ephemeral-storage.
+  Normal   Killing  11m   kubelet  spec.containers{kube-tools}: Stopping container kube-tools
+EOF
+
+echo
+echo "Consider filtering on krew plugin names ?"
+exit
 kubectl krew install $( kubectl krew search | awk '!/^NAME/ { print $1; }' )
 
